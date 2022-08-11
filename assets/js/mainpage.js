b0595f8f9a95bfad6f3f95d35939acb3b9e8f831
Raitis Barkans <raitisbarkans@gmail.com>
2022-08-11 21:23:51 +0300

    Add chess board dynamic generation

--- assets/js/mainpage.js
+++ assets/js/mainpage.js
@@ -1,4 +1,67 @@
-var temp = Array.from(document.getElementsByClassName('chess-field'))
+var pieceSet = {
+    white: {
+        a1: "assets\img\white_rook.svg",
+        a2:"assets\img\black_knight.svg",
+        a3:"assets\img\white_bishop.svg",
+        a4:"assets\img\white_king.svg",
+        a5:"assets\img\black_queen.svg",
+        a6:"assets\img\white_bishop.svg",
+        a7: "assets\img\black_knight.svg",
+        a8: "assets\img\white_rook.svg"
+    },
+    black:{
+
+    }
+}
 
 
-temp.forEach(el => console.log(el))
+flipBoard = color => {
+    var whiteBoard = document.getElementById("white-board");
+    var blackBoard = document.getElementById("black-board");
+
+    console.log(whiteBoard); console.log(blackBoard);
+
+    if(whiteBoard.style.display.includes("none")){
+        blackBoard.style.display = "none";
+        whiteBoard.style.display = "";
+    } else if(blackBoard.style.display.includes("none")){
+        blackBoard.style.display = "";
+        whiteBoard.style.display = "none";
+    }
+}
+
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+generateBoard = color => {
+    var mainView = document.getElementById("app");
+    var whiteBoard = document.createElement("div");
+
+    whiteBoard.classList.add("container-fluid", "px-4", "chess-board");
+    whiteBoard.setAttribute('id','white-board');
+    whiteBoard.style.display = "";
+    
+    for(let i = 0,whiteRowNo = 7; i < 8; i++,whiteRowNo--){
+        let tempRow = document.createElement("div");
+        tempRow.classList.add("row");
+        let blackRowNo = i + 1;
+
+        for(let j = 0, blackLetterIndex = 7; j < 8; j++,blackLetterIndex--){
+            let tempField = document.createElement("div");
+            tempField.classList.add("col-sm", "chess-field");
+            if((j - i) % 2 !== 0){   // Generate alternating chess pattern
+                tempField.classList.add("bg-black");
+            }
+            if(color == "black")
+                tempField.innerText = letters[blackLetterIndex] + blackRowNo;
+            else
+                tempField.innerText = letters[j] + (whiteRowNo + 1);
+            tempRow.appendChild(tempField);
+        }
+
+        whiteBoard.appendChild(tempRow);
+    }
+
+    mainView.appendChild(whiteBoard);
+}
+
+generateBoard("white");
--- index.php
+++ index.php
@@ -6,8 +6,6 @@
         <link rel="stylesheet" href="assets\css\mainpage.css">
     </head>
         <body>
-            <script src="https://unpkg.com/vue@3"></script>
-            
             <div id="app">
                 <header>
                     <nav class="navbar navbar-expand-lg navbar-light bg-white">
@@ -32,101 +30,8 @@
                         </div>
                     </nav>
                 </header>
-                <div class = "container-fluid px-4 chess-board">
-                    <div class = "row">
-                        <div class = "col-sm chess-field">a8</div>
-                        <div class = "col-sm bg-black chess-field">b8</div>
-                        <div class = "col-sm chess-field">c8</div>
-                        <div class = "col-sm bg-black chess-field">d8</div>
-                        <div class = "col-sm chess-field">e8</div>
-                        <div class = "col-sm bg-black chess-field">g8</div>
-                        <div class = "col-sm chess-field">f8</div>
-                        <div class = "col-sm bg-black chess-field">h8</div>
-                    </div>
-                    <div class = "row">
-                        <div class = "col-sm bg-black chess-field">a7</div>
-                        <div class = "col-sm chess-field">b7</div>
-                        <div class = "col-sm bg-black chess-field">c7</div>
-                        <div class = "col-sm chess-field">d7</div>
-                        <div class = "col-sm bg-black chess-field">e7</div>
-                        <div class = "col-sm chess-field">f7</div>
-                        <div class = "col-sm bg-black chess-field">g7</div>
-                        <div class = "col-sm chess-field">h7</div>
-                    </div>
-                    <div class = "row">
-                        <div class = "col-sm chess-field">a6</div>
-                        <div class = "col-sm bg-black chess-field">b6</div>
-                        <div class = "col-sm chess-field">c6</div>
-                        <div class = "col-sm bg-black chess-field">d6</div>
-                        <div class = "col-sm chess-field">e6</div>
-                        <div class = "col-sm bg-black chess-field">f6</div>
-                        <div class = "col-sm chess-field">g6</div>
-                        <div class = "col-sm bg-black chess-field">h6</div>
-                    </div>
-                    <div class = "row">
-                        <div class = "col-sm bg-black chess-field">a5</div>
-                        <div class = "col-sm chess-field">b5</div>
-                        <div class = "col-sm bg-black chess-field">c5</div>
-                        <div class = "col-sm chess-field">d5</div>
-                        <div class = "col-sm bg-black chess-field">e5</div>
-                        <div class = "col-sm chess-field">f5</div>
-                        <div class = "col-sm bg-black chess-field">g5</div>
-                        <div class = "col-sm chess-field">h5</div>
-                    </div>
-                    <div class = "row">
-                        <div class = "col-sm chess-field">a4</div>
-                        <div class = "col-sm bg-black chess-field">b4</div>
-                        <div class = "col-sm chess-field">c4</div>
-                        <div class = "col-sm bg-black chess-field">d4</div>
-                        <div class = "col-sm chess-field">e4</div>
-                        <div class = "col-sm bg-black chess-field">f4</div>
-                        <div class = "col-sm chess-field">g4</div>
-                        <div class = "col-sm bg-black chess-field">h4</div>
-                    </div>
-                    <div class = "row">
-                        <div class = "col-sm bg-black chess-field">a3</div>
-                        <div class = "col-sm chess-field">b3</div>
-                        <div class = "col-sm bg-black chess-field">c3</div>
-                        <div class = "col-sm chess-field">d3</div>
-                        <div class = "col-sm bg-black chess-field">e3</div>
-                        <div class = "col-sm chess-field">f3</div>
-                        <div class = "col-sm bg-black chess-field">g3</div>
-                        <div class = "col-sm chess-field">h3</div>
-                    </div>
-                    <div class = "row">
-                        <div class = "col-sm chess-field">a2</div>
-                        <div class = "col-sm bg-black chess-field">b2</div>
-                        <div class = "col-sm chess-field">c2</div>
-                        <div class = "col-sm bg-black chess-field">d2</div>
-                        <div class = "col-sm chess-field">e2</div>
-                        <div class = "col-sm bg-black chess-field">f2</div>
-                        <div class = "col-sm chess-field">g2</div>
-                        <div class = "col-sm bg-black chess-field">h2</div>
-                    </div>
-                    <div class = "row">
-                        <div class = "col-sm bg-black chess-field">a1</div>
-                        <div class = "col-sm chess-field">b1</div>
-                        <div class = "col-sm bg-black chess-field">c1</div>
-                        <div class = "col-sm chess-field">d1</div>
-                        <div class = "col-sm bg-black chess-field">e1</div>
-                        <div class = "col-sm chess-field">f1</div>
-                        <div class = "col-sm bg-black chess-field">g1</div>
-                        <div class = "col-sm chess-field">h1</div>
-                    </div>
-                </div>
             </div>
             <script src="assets\js\mainpage.js"></script>
-            <script>
-                const { createApp } = Vue
-
-                createApp({
-                    data() {
-                        return {
-                            message: 'Hello Vue!'
-                        }
-                    }
-                }).mount('#app')
-            </script>
         </body>
 </html>
 
